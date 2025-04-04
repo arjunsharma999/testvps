9324b954655c73d38827fcbe670ee794402dfeb8
arjunsharma999 <arjunsharmarke99@gmail.com>
2025-04-04 16:56:17 +0530

    hello

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 def check_url(url):
-    print("hello")
+    print("helo")
     try:
         response = requests.get(url, timeout=10)
         print(f"Response status code: {response.status_code}")
@@ -10,5 +10,5 @@ def check_url(url):
         print(f"Error sending request: {e}")
 
 new_server_ip = "103.174.102.113"
-url = f"http://{new_server_ip}/main.py?id=TIH_AGRI1&data=0912221305,21.27,24.88,00320,0018,0026,0012,06.99,0.3,12.4"
+url = f"https://{new_server_ip}/home.php?id=TIH_AGRI1&data=0912221305,21.27,24.88,00320,0018,0026,0012,06.99,0.3,12.4"
 check_url(url)
