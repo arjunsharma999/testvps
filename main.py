e0139c10fdc25fb1aaac8cef807b7e232a91b283
arjunsharma999 <arjunsharmarke99@gmail.com>
2025-04-04 16:26:10 +0530

    https ninjs

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import requests
 
 def check_url(url):
     try:
-        response = requests.get(url)
-        if response.text == "ACK":
-            print("Received ACK from the server")
-        else:
-            print("Did not receive ACK from the server. Response was:", response.text)
+        response = requests.get(url, timeout=10)
+        print(f"Response status code: {response.status_code}")
+        print(f"Response text: {response.text}")
     except requests.exceptions.RequestException as e:
-        print("Error sending request:", e)
+        print(f"Error sending request: {e}")
 
 new_server_ip = "103.174.102.113"
 url = f"https://{new_server_ip}/home.php?id=TIH_AGRI1&data=0912221305,21.27,24.88,00320,0018,0026,0012,06.99,0.3,12.4"
